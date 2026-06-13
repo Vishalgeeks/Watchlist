1381399309485fc8ea0dfb35eea00de40c926756
Vishal <vishalsingh4328m@gmail.com>
2026-06-13 09:52:56 +0530

    feat: config added

---
+++ config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	ServerPort string
+}
+
+// agr .env file ni hoga to ye basis pe run krenge
+func Load() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using system env")
+	}
+	return &Config{
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", ""),
+		DBName:     getEnv("DB_NAME", "watchlist_db"),
+		ServerPort: getEnv("SERVER_PORT", "8080"),
+	}
+}
+
+// Db connect krrew h
+func (c *Config) DBConnectionString() string {
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		c.DBHost, c.DBPort, c.DBUser, c.DBPassword, c.DBName,
+	)
+}
+
+// ,env file k data key value pair me OS env check krta h ni mila to fallback use krta h
+
+func getEnv(key, fallback string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return fallback
+}
