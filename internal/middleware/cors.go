b9aa61551e5a443d04624e84b94d6c92062bfc47
ShariquaHazra <shariquahazra@gmail.com>
2026-06-17 22:31:34 +0530

    Update CORS headers to include ngrok warning

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -11,10 +11,10 @@ func CORSMiddleware() gin.HandlerFunc {
 		fmt.Println("CORS Middleware Executed")
 
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		//c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set(
 			"Access-Control-Allow-Headers",
-			"Content-Type, Content-Length, Accept-Encoding, Authorization, X-CSRF-Token",
+			"Content-Type, Content-Length, Accept-Encoding, Authorization, X-CSRF-Token, ngrok-skip-browser-warning",
 		)
 		c.Writer.Header().Set(
 			"Access-Control-Allow-Methods",
